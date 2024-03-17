baa38e3fadbc5bbc8f7cebf1ab5483c7bc644b4b
tarun <tarun@DESKTOP-83VNE9M>
2024-03-17 22:14:01 +0530

    first commit

---
+++ ExampleProject1/src/main/java/com/example/service/DepartmentService.java
@@ -0,0 +1,12 @@
+package com.example.service;
+
+import com.example.entity.Department;
+
+public interface DepartmentService {
+
+	static Department saveDepartment(Department department) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
