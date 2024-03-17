85f8c5a0669a1ae011f2219190acf579d4c9305e
tarun <tarun@DESKTOP-83VNE9M>
2024-03-17 20:20:02 +0530

    first commit

---
+++ ExampleProject1/src/main/java/com/example/controller/DepartmentController.java
@@ -0,0 +1,37 @@
+package com.example.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+import com.example.entity.Department;
+import com.example.service.DepartmentService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+@RestController
+public class DepartmentController {
+ 
+      @Autowired
+     private DepartmentService departmentService;
+
+	public DepartmentService getDepartmentService() {
+		return departmentService;
+	}
+
+	public void setDepartmentService(DepartmentService departmentService) {
+		this.departmentService = departmentService;
+	}
+
+     @PostMapping("path")
+     public Department saveDepartment(@RequestBody Department department) {
+         //TODO: process POST request
+         
+         return DepartmentService.saveDepartment(department);
+         
+      
+     }
+     
+	
+	
+}
